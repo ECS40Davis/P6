15f7be649be43dba1472e5e09aafadea8b329293
MaJessica9 <jamma@ucdavis.edu>
2015-05-20 22:54:44 -0700

    Create airport.h

---
+++ airport.h
@@ -0,0 +1,17 @@
+#ifndef AIRPORT_H
+#define	AIRPORT_H
+
+#include "city.h"
+
+class Airport: public City
+{
+public:
+    Airport();
+    Airport(const Airport& orig);
+    virtual ~Airport();
+private:
+
+};
+
+#endif	/* AIRPORT_H */
+
