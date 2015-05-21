2acc1e34014be803cbf343627893fc6adba52459
MaJessica9 <jamma@ucdavis.edu>
2015-05-20 18:42:22 -0700

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,113 +1,122 @@
-// Author: Sean Davis
+// Author: Jessica Ma
 #include <iostream>
 #include <cstdlib>
 #include "list.h"
 using namespace std;
 
-int List::count = 0;
+template <typename T>
+int List<T>::count = 0;
 
-template <class T>
-ListNode::ListNode(const City &c, ListNode *node) : city(c), next(node)
+// ListNode::ListNode(const City &c, ListNode *node) : city(c), next(node)
+template <typename T>
+ListNode<T>::ListNode(const T &d, ListNode<T> *n) : data(d), next(n)
 {
-    
+  
 }  // ListNode())
 
-template <class T>
-List<T>::List() : head(NULL), tail(NULL)
+
+template <typename T>
+List<T>::List() : head(NULL), tail(NULL) 
 {
 } // List()
 
-template <class T>
-List<T>::~List()
+
+template <typename T>
+List<T>::~List() 
 {
-    for (ListNode *ptr = head; ptr; ptr = head)
-    {
-        head = ptr->next;
-        delete ptr;
-    }  // for each node
+  for (ListNode<T> *ptr = head; ptr; ptr = head)
+  {
+    head = ptr->next;
+    delete ptr;
+  }  // for each node
 } // ~List()
 
-template <class T>
-int List::getCount()
+
+template <typename T>
+int List<T>::getCount()
 {
-    return count;
+  return count;
 }  // getCount()
 
-template <class T>
-List&  List::operator += (const City &rhs)
+template <typename T>
+List<T>&  List<T>::operator += (const T &rhs)
 {
-    if (tail)
-        tail = tail->next = new ListNode(rhs, NULL);
-    else // an empty list
-        head = tail = new ListNode(rhs, NULL);
-    
-    count++;
-    return *this;
+  if (tail)
+    tail = tail->next = new ListNode<T>(rhs, NULL);
+  else // an empty list
+    head = tail = new ListNode<T>(rhs, NULL);
+  
+  count++;
+  return *this;
 }  // operator+=
 
-template <class T>
-List& List::operator -= (const City &rhs)
+
+// only changed city to data?
+template <typename T>
+List<T>& List<T>::operator -= (const T &rhs)
 {
-    ListNode *ptr, *prev = NULL;
+  ListNode<T> *ptr, *prev = NULL;
+  
+  for (ptr = head; ptr && ! (ptr->data == rhs); ptr = ptr->next)
+    prev = ptr;
+  
+  if (ptr)
+  {
+    count--;
     
-    for (ptr = head; ptr && ! (ptr->city == rhs); ptr = ptr->next)
-        prev = ptr;
-    
-    if (ptr)
+    if (!prev)  // found at front of list
     {
-        count--;
-        
-        if (!prev)  // found at front of list
-        {
-            head = ptr->next;
-            
-            if (!head)  // head == NULL so now empty list
-                tail = NULL;
-        }  // if removing first node
-        else // removing node after the first
-        {
-            prev->next = ptr->next;
-            
-            if (tail == ptr)
-                tail = prev;
-        }  // else removing after the first node
-        
-        delete ptr;
-    }  // if found
+      head = ptr->next;
+      
+      if (!head)  // head == NULL so now empty list
+        tail = NULL;
+    }  // if removing first node
+    else // removing node after the first
+    {
+      prev->next = ptr->next;
+      
+      if (tail == ptr)
+        tail = prev;
+    }  // else removing after the first node
     
-    return *this;
+    delete ptr;
+  }  // if found 
+
+  return *this;
 }  // operator -=
 
-template <class T>
-const City& List::operator [] (int index) const
-{
-    int pos = 0;
-    ListNode *ptr;
-    
-    for (ptr = head; ptr && pos != index; ptr = ptr->next, pos++);
-    
-    if (!ptr)
-    {
-        cout << "Error in array indexing!\n";
-        exit(1);
-    }  // if index beyond end of list
-    
-    return ptr->city;
-}  // const operator[]
 
-template <class T>
-City& List::operator [] (int index)
+template <typename T>
+const T& List<T>::operator [] (int index) const
 {
-    int pos = 0;
-    ListNode *ptr;
-    
-    for (ptr = head; ptr && pos != index; ptr = ptr->next, pos++);
-    
-    if (!ptr)
-    {
-        cout << "Error in array indexing!\n";
-        exit(1);
-    } // if index beyond end of the array.
-    
-    return ptr->city;
+  int pos = 0;
+  ListNode<T> *ptr;
+  
+  for (ptr = head; ptr && pos != index; ptr = ptr->next, pos++);
+   
+  if (!ptr)
+  {
+     cout << "Error in array indexing!\n";
+     exit(1);
+  }  // if index beyond end of list
+
+  return ptr->data; 
+}  // const operator[]
+ 
+ 
+template <typename T>
+T& List<T>::operator [] (int index)
+{
+  int pos = 0;
+  ListNode<T> *ptr;
+   
+  for (ptr = head; ptr && pos != index; ptr = ptr->next, pos++);
+   
+  if (!ptr)
+  {
+    cout << "Error in array indexing!\n";
+    exit(1);
+  } // if index beyond end of the array.
+
+  return ptr->data; 
 }  // non-const operator[]
